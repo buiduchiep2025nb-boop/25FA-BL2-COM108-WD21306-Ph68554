1df56d230254bfcf4cc3f1d77e2e1647e12eae37
PH68554 <buiduchiep2025nb@gmail.com>
2025-11-28 11:27:03 +0700

    Bài code hôm nay

--- CodeTaiLop_COM108/CodeTaiLop_COM108/Program.c
+++ CodeTaiLop_COM108/CodeTaiLop_COM108/Program.c
@@ -1,31 +1,45 @@
-// CConsoleApplication.c : file nay chua 'main' function. 
-// Chuong trinh phan mem Bat dau thuc thi & Ket thuc o day.
+
 
 #include <stdio.h>
+void lapChucNang(int chonChucNang)
 
+
+{
+    int tiepTuc = 1;
+    while (tiepTuc == 1)
+    {
+        switch (chonChucNang)
+        {
+        case 1://goi Ten Ham
+            break;
+        case 2://goi Ten Ham
+            break;
+        case 3://goi Ten Ham
+            break;
+       
+        default:
+            
+                printf("Chon lai tu [1-3]");
+            
+        }
+        printf("Tiep tuc chuc nang nay khong ?  [1=Co  |  0=Khong]");
+        scanf("%d", tiepTuc);
+        system("cls"); // clear screen
+    }
+}
 int main()
 {
-    printf("Hi there!\n");
+    int chonChucNang;
+    do
+    {
+        printf("Moi chon chuc nang: \n");
+        printf("Menu\n");
+        printf("1.Kiem Tra So Nguyen\n");
+        printf("2.Uoc chung va Boi chung\n");
+        printf("3.Tinh Tien\n");
+        printf("Moi chon chuc nang[1-3]\n");
+        scanf("%d", &chonChucNang);
+        if(chonChucNang!=0){ lapChucNang(chonChucNang); }
+        
+    } while (chonChucNang != 0);
 }
-
-// Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
-
-// Danh cho nguoi moi bat dau:
-//   1. Mo "View" > "Solution Explorer"
-//          de them/quan ly cac files
-//   2. Mo "View" > "Output"
-//          de kiem tra "build output" va "cac thong bao khac"
-//   3. Mo "View" > "Error List"
-//          de xem loi
-//   4. Chon Project > "Add" > "New Item"
-//          de tao moi file code,
-//      hoac Project > "Add" > "Existing Item"
-//          de them file da ton tai vao project nay.
-//   5. Sau nay, de mo lai project nay,
-//          chon "File > Open > Project" va chon file *.sln
-//      Co the xem thu muc project, noi luu file *.sln bang cach
-//          trong cua so "Solution Explorer"
-//          bam chuot phai vao "Solution"
-//          chon "Open Folder in File Explorer"
-
-// GV: AnhTT184
