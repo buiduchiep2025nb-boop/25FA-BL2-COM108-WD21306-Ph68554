b4be44cdee28e8e012fd8f3fb683fe17a1433d40
PH68554 <buiduchiep2025nb@gmail.com>
2025-12-03 11:18:12 +0700

    code hom nay

--- CodeTaiLop_COM108/CodeTaiLop_COM108/Program.c
+++ CodeTaiLop_COM108/CodeTaiLop_COM108/Program.c
@@ -17,10 +17,11 @@ void lapChucNang(int chonChucNang)
             break;
         case 3:tinhTien;
             break;
-       
+        case 4:demoMang2Chieu;
+            break;
         default:
             
-                printf("Chon lai tu [1-3]");
+                printf("Chon lai tu [1-4]");
             
         }
         printf("Tiep tuc chuc nang nay khong ?  [1=Co  |  0=Khong]");
@@ -31,10 +32,65 @@ void lapChucNang(int chonChucNang)
 void kiemTraSoNguyen(){}
 void uocChungVaBoiChung(){}
 void tinhTien(){}
-
-       
-int main()
+void demoMang2Chieu() 
 {
-    
+    int array[2][3];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("Mang[%d][%d]=", i, j);
+            scanf("%d", &array[i][j]);
+
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                printf("%d, ", array[i][j]);
+            }
+            printf("\n");
+
+        }
+    }
 }
+void demoString()
+{
+    char name[10] = { 'A','n','h' };//"Anh"
+    //get(name);>fgets();
+    for (int i = 0; i < 3; i++)
+    {
+        scanf(" %c", &name[i]);
+    }
+    // xuat du lieu = for+printf
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%c", name[i]);
+    }
+    puts(name);
+}
+int main()
+
+{
+    int chonChucNang;
+
+
+    do
+    {
+        printf("menu\n");
+        printf("1. Kiem Tra so nguyen \n");
+        printf("2. mang 2 chieu\n");
+        printf("3.String \n");
+        printf("menu\n");
+        printf("menu\n");
+        printf("menu\n");
+        printf("menu\n");
+        printf("menu\n");
+
+        scanf_s("%d", &chonChucNang);
+        lapChucNang(chonChucNang);
+
+    } while (chonChucNang != 0);
+} 
 
